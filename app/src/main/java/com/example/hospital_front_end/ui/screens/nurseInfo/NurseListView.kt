9f1b9ec6f621c33e6f8c689988456e338e7d26f2
marchyxd <jiahaoliu8888@gmail.com>
2024-12-04 17:41:44 +0100

    new Nurse class and change list to ArrayList

--- app/src/main/java/com/example/hospital_front_end/ui/screens/nurseInfo/NurseListView.kt
+++ app/src/main/java/com/example/hospital_front_end/ui/screens/nurseInfo/NurseListView.kt
@@ -1,6 +1,7 @@
 package com.example.hospital_front_end.ui.screens.nurseInfo
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -18,9 +19,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.hospital_front_end.R
+import com.example.hospital_front_end.model.nurse.Nurse
+import java.util.Date
 
 @Composable
-fun NurseList(nurseList: List<String>, onBack: () -> Unit) {
+fun NurseList(nurseList: ArrayList<Nurse>, onBack: () -> Unit) {
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -47,40 +50,93 @@ fun NurseList(nurseList: List<String>, onBack: () -> Unit) {
 
         Spacer(modifier = Modifier.height(28.dp))
 
-        LazyColumn {
+        LazyColumn(
+            contentPadding = PaddingValues(bottom = 8.dp)
+        ) {
             items(nurseList) { nurse ->
-                Text(
-                    text = nurse,
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(vertical = 4.dp)
-                )
+
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.user),
+                        contentDescription = "Icon",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        Row(
+
+                        ) {
+                            Text(
+                                text = nurse.getName(),
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = nurse.getLastName(),
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+                        Text(
+                            text = "Email: ${nurse.getEmail()}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Text(
+                            text = "Edad: ${nurse.getAge()}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Text(
+                            text = "Fecha: ${nurse.getRegisterDate()}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
             }
+
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = onBack,
+                    ) {
+                        Text("Back")
+                    }
+                }
+            }
+
+
         }
 
-        Spacer(modifier = Modifier.height(26.dp))
 
-        Button(onClick = onBack, modifier = Modifier.align(Alignment.CenterHorizontally)) {
-            Text("Back")
-        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MyNurseListPreview() {
-    val nurseList = listOf(
-        "Juan Pérez",
-        "María Gómez",
-        "Luis Fernández",
-        "Ana López",
-        "Carlos Díaz",
-        "Sofía Ramírez",
-        "Diego Herrera",
-        "Paula Ortiz",
-        "Andrés Castro",
-        "Elena Vargas",
-        "Miguel Ángel Rodríguez",
-        "Natalia Morales"
+    val nurseList = arrayListOf(
+        Nurse(1, "Juan", "Pérez", 30, "juan.perez@example.com", Date()),
+        Nurse(2, "María", "Gómez", 28, "maria.gomez@example.com", Date()),
+        Nurse(3, "Luis", "Fernández", 35, "luis.fernandez@example.com", Date()),
+        Nurse(4, "Ana", "López", 40, "ana.lopez@example.com", Date()),
+        Nurse(5, "Carlos", "Díaz", 29, "carlos.diaz@example.com", Date()),
+        Nurse(6, "Sofía", "Ramírez", 32, "sofia.ramirez@example.com", Date()),
+        Nurse(7, "Diego", "Herrera", 27, "diego.herrera@example.com", Date()),
+        Nurse(8, "Paula", "Ortiz", 33, "paula.ortiz@example.com", Date()),
+        Nurse(9, "Andrés", "Castro", 31, "andres.castro@example.com", Date()),
+        Nurse(10, "Elena", "Vargas", 25, "elena.vargas@example.com", Date())
     )
     NurseList(nurseList = nurseList, onBack = { /* Simulated Back Action */ })
 }
