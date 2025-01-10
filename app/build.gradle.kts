cfda7e60b99410b752eeee0d5e56055524f294e3
marchyxd <jiahaoliu8888@gmail.com>
2025-01-10 16:23:04 +0100

    Done adding new Dependencies and AndroidManifest configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,10 @@ android {
 dependencies {
     // Koin Core para Android
     implementation(libs.koin.android)
-
+    //new dependencies for async back end
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     // Koin para Jetpack Compose
     implementation(libs.koin.androidx.compose)
     implementation(libs.compose)
