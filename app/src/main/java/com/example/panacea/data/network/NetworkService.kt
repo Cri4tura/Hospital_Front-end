e85f5beae7f4445c49c7299c9dd19b09bf20ede4
Ashla-StarWars <enric.160493@gmail.com>
2025-01-30 23:51:14 +0100

    trying flows and vm state

--- app/src/main/java/com/example/panacea/data/network/NetworkService.kt
+++ app/src/main/java/com/example/panacea/data/network/NetworkService.kt
@@ -17,9 +17,10 @@ class NetworkServices() {
         val allNurses = mutableListOf<Nurse>()
 
         // TODO : Descomentar segun el PC
-        //val response = client.get("http://10.118.4.237:8080/nurse")           // MARTINA
-        //val response = client.get("http://10.118.3.254:8080/nurse")           // JIAHAO
-        val response = client.get("http://192.168.1.134:8080/nurse")    // ENRIC
+        // val response = client.get("http://10.118.4.237:8080/nurse")          // MARTINA
+        // val response = client.get("http://10.118.3.254:8080/nurse")          // JIAHAO
+        // val response = client.get("http://10.0.2.2:8080/nurse")              // ENRIC PORTATIL
+        val response = client.get("http://192.168.1.134:8080/nurse")    // ENRIC DESKTOP
 
         val nurseResponse: NurseResponse = jsonData.decodeFromString(response.bodyAsText())
 
@@ -38,7 +39,9 @@ class NetworkServices() {
     }
 
     suspend fun getNurseById(nurseId: Int): Nurse {
-        val response = client.get("http://10.0.2.2:8080/nurse/id/$nurseId")
+        // val response = client.get("http://10.0.2.2:8080/nurse/id/$nurseId")              // ENRIC PORTATIL
+        val response = client.get("http://192.168.1.134:8080/nurse/id/$nurseId")    // ENRIC DESKTOP
+
         val nurseResponse: Nurse = jsonData.decodeFromString<Nurse>(response.bodyAsText())
 
         println("Nurse: $nurseResponse")
